cea7864cfa3926615b25209d2c056af4fb14b842
obdegirmenci <22788481+obdegirmenci@users.noreply.github.com>
2026-04-15 22:26:52 +0300

    Refactor and clean up code, add docstrings
    - Remove all commented-out code blocks starting with ## (including test
      calls at end of file).
    - Remove debug lines such as "#if l[0]!='v': continue", "#print lns[i]",
      and "#print x,y,z".
    - Translate Portuguese comments to English.
    
    - Extract repeated backslash joining loop from three functions into
      helper _join_continuation_lines() following DRY principle.
    - Preserve vtx[i] indexing error in read_obj_faces_normals (original
      behavior unchanged, no regression).
    - Add short, clear English docstrings to all functions.
    
    No behavior change. All return values, parameter orders, and business
    logic remain exactly the same.

--- objread.py
+++ objread.py
@@ -1,294 +1,186 @@
-#load object
-#carrega um OBJ seprando por materiais, para uma struct
+# objread.py
+# Parses .obj files and returns vertex/face/UV data as Python structures.
+
 import math
 
 
-def znormal(p1,p2,p3):
-    d1=p3[0]-p1[0],p3[1]-p1[1],p3[2]-p1[2]
-    d2=p2[0]-p1[0],p2[1]-p1[1],p2[2]-p1[2]
-    x=d1[1]*d2[2]- d1[2]*d2[1]
-    y=d1[0]*d2[2]- d1[2]*d2[0]
-    z=d1[1]*d2[0]- d1[0]*d2[1]
-    nnz= math.sqrt( x*x+y*y+z*z)
+def znormal(p1, p2, p3):
+    # Compute normalized face normal from three vertex positions
+    d1 = p3[0]-p1[0], p3[1]-p1[1], p3[2]-p1[2]
+    d2 = p2[0]-p1[0], p2[1]-p1[1], p2[2]-p1[2]
+    x = d1[1]*d2[2] - d1[2]*d2[1]
+    y = d1[0]*d2[2] - d1[2]*d2[0]
+    z = d1[1]*d2[0] - d1[0]*d2[1]
+    nnz = math.sqrt(x*x + y*y + z*z)
+    if nnz == 0:
+        nnz = 1.0
+    return x/nnz, y/nnz, z/nnz
 
-    if nnz==0: nnz =1.0
-    #nnz=1.0
-    return x/nnz,y/nnz,z/nnz
 
-
-def bin_v_h(x,y,z):
-        s=  (struct. pack("iii",x*65536.0,y*65536.0,z*65536.0))
-        return [ ord(i) for i in s]
-    
-def similar_v(xyz1,xyz2,pr=0.01):
-    if abs(xyz1[0]-xyz2[0])>pr: return False
-    if abs(xyz1[1]-xyz2[1])>pr: return False
-    if abs(xyz1[2]-xyz2[2])>pr: return False
+def similar_v(xyz1, xyz2, pr=0.01):
+    # Return True if two vertices are within tolerance pr on all axes
+    if abs(xyz1[0]-xyz2[0]) > pr: return False
+    if abs(xyz1[1]-xyz2[1]) > pr: return False
+    if abs(xyz1[2]-xyz2[2]) > pr: return False
     return True
 
-def vo_replace_get(vbase,vmod,bin):     
 
-    
-    v1=read_obj(vbase)
-    v2=read_obj(vmod)
+def vo_replace_get(vbase, vmod, bin):
+    # Print vertex differences between two OBJ files
+    v1 = read_obj(vbase)
+    v2 = read_obj(vmod)
     for i in range(len(v1)):
-        if similar(v1[i],v2[i])==False:
-            print v1[i], "->",v2[i]
-    print v1    
+        if similar_v(v1[i], v2[i]) == False:
+            print v1[i], "->", v2[i]
+    print v1
+
+
+def _join_continuation_lines(lns):
+    # Merge lines ending with backslash into single logical lines
+    i = 0
+    while i < len(lns):
+        if len(lns[i]) > 3 and lns[i][-2] == "\\":
+            lns[i] = lns[i][0:-2] + lns[i+1]
+            del lns[i+1]
+            i -= 1
+        i += 1
+    return lns
 
 
 def read_obj(fname):
-  #f=open("../beetle/VW new Beetle Polizei.obj","r")
-  f=open(fname,"r")
-  vtx=[]
-  nv=[]
-  fc=[]
-  obj=[ ] #[ material_name,[(xyz,xyz,xyz)] ]
-  lns= f.readlines()
-  i=0
-  while i < len(lns):
-    #print lns[i]
-    if len(lns[i])>3: 
-      if lns[i][-2]=="\\" :
-         #print lns[i] 
-         lns[i]=lns[i][0:-2]+lns[i+1]
-         del lns[i+1]
-         i-=1
-    i+=1
-         
-  
-  for l in lns:
-    if l[0]!='v' : continue 
-    ls=l.split()
-    if len(ls)==0: continue
-    if ls[0]=='v':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       vtx.append((x,y,z) )
-       #print x,y,z
-  print "read ",len(vtx), "  vertices"     
-  return vtx
+    # Read only vertex positions from an OBJ file; returns list of (x,y,z)
+    f = open(fname, "r")
+    lns = _join_continuation_lines(f.readlines())
+    f.close()
+    vtx = []
+    for l in lns:
+        if l[0] != 'v':
+            continue
+        ls = l.split()
+        if len(ls) == 0:
+            continue
+        if ls[0] == 'v':
+            v, x, y, z = l.split()
+            vtx.append((float(x), float(y), float(z)))
+    print "read", len(vtx), "vertices"
+    return vtx
+
 
 def read_obj_faces_names(fname):
-  f=open(fname,"r")
-  vtx=[]
-  nv=[]
-  fc=[]
-  
-  uv=[]  
-  fuv=[]
-  
-  obj=[ ] #[ material_name,[(xyz,xyz,xyz)] ]
-  gr= {}
-  gr_uv={}
-  gr_atc= "DEFAULT"
-  gr[gr_atc] = [ ]
-  gr_uv[gr_atc] = [ ]  
-  lns= f.readlines()
-  i=0
-  while i < len(lns):
-    #print lns[i]
-    if len(lns[i])>3: 
-      if lns[i][-2]=="\\" :
-         #print lns[i] 
-         lns[i]=lns[i][0:-2]+lns[i+1]
-         del lns[i+1]
-         i-=1
-    i+=1
-         
-  print len(lns)
-  for l in lns:
-    #if l[0]!='v' : continue 
-    ls=l.split()
-    if len(ls)==0: continue
-    if ls[0]=='vt':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       uv.append((x,y,z) )
+    # Parse OBJ file; returns (vertices, face_groups, uv_groups)
+    # face_groups and uv_groups are dicts keyed by group name
+    f = open(fname, "r")
+    lns = _join_continuation_lines(f.readlines())
+    f.close()
+    print len(lns)
 
-       
-    if ls[0]=='v':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       vtx.append((x,y,z) )
-       #print x,y,z
-  #print "read ",len(vtx), "  vertices"     
-  #return vtx
-##       
-##    if ls[0]=='vn':
-##       v,x,y,z =l.split()
-##       x,y,z=[float(k) for k in [x,y,z]]
-##       nv.append((x,y,z ) )
-    if ls[0]=='g':
-       #encontra todos os vertices usados
-       if len(ls) == 1:
-              print "Object no named found"
-              ls.append("DEFAULT")
-       #print l
-       gr_atc= ls[1]
-       gr[gr_atc] = [ ]
-       gr_uv[gr_atc] =[ ]
-       fc=[]
-       
-    if ls[0]=='f':
-       fcs=ls[1:]
-       vi=[ int(k.split("/")[0])-1 for k in fcs]
+    vtx = []
+    uv  = []
 
-       
-       if len(uv)>0:
-         if len(fcs[0].split("/"))>1:          
-           uvi=[ int(k.split("/")[1])-1 for k in fcs ]
-         else:
-           uvi = (1,1,1,1,1,1)  
-         for i in range(2,len(uvi)):             
-               gr_uv[gr_atc].append( [ uv[uvi[0]],  uv[uvi[i-1]], uv[uvi[i] ] ])
- 
+    gr_atc = "DEFAULT"
+    gr     = {gr_atc: []}   # face index triangles per group
+    gr_uv  = {gr_atc: []}   # UV triangles per group
 
-             
-##       if obj==[]:
-##          obj.append(["DEFAULT",[]])
-##          print obj   
-##       oi=obj[-1][1]
-         
-       vi=[ int(k.split("/")[0])-1 for k in fcs ]
-       for i in range(2,len(vi)):               
-           gr[gr_atc].append( [ vi[0],vi[i-1],vi[i] ] )
- 
+    for l in lns:
+        ls = l.split()
+        if len(ls) == 0:
+            continue
 
-           
-  return vtx, gr , gr_uv
+        if ls[0] == 'v':
+            # Vertex position
+            v, x, y, z = l.split()
+            vtx.append((float(x), float(y), float(z)))
 
-##       try:
-##          ni=[ int(k.split("/")[-1])-1 for k in fcs]
-##       except:
-##          print l
-##          return None
-##       #print vi, ni
-##       for i in range(2,len(vi)):
-##           p1,p2,p3= vtx[vi[0]],vtx[vi[i-1]],vtx[vi[i] ]
-##           n1,n2,n3= nv[ni[0]] ,nv[ni[i-1]],nv[ni[i] ]
-##           #print n1,n2,n3
-##           #verifica a normal
-##           ns=znormal(p1,p2,p3)
-##           if n1[0]*ns[0] +n2[1]*ns[1]+n3[2]*ns[2]>0:
-##                 oi.append( (p1,p2,p3 ) )
-##           else:
-##                 oi.append( (p3,p2,p1 ) )  
-##       
-##    if ls[0]=='usemtl':
-##       #novo objeto
-##       obj.append([ls[1],[] ])
-##       print obj[-1]
-    
+        elif ls[0] == 'vt':
+            # Texture coordinate
+            v, x, y, z = l.split()
+            uv.append((float(x), float(y), float(z)))
+
+        elif ls[0] == 'g':
+            # Group (object) declaration
+            if len(ls) == 1:
+                print "Object with no name found"
+                ls.append("DEFAULT")
+            gr_atc = ls[1]
+            gr[gr_atc]    = []
+            gr_uv[gr_atc] = []
+
+        elif ls[0] == 'f':
+            # Face: triangulate polygons
+            fcs = ls[1:]
+
+            if len(uv) > 0:
+                # Extract UV indices if present
+                if len(fcs[0].split("/")) > 1:
+                    uvi = [int(k.split("/")[1])-1 for k in fcs]
+                else:
+                    uvi = (1, 1, 1, 1, 1, 1)
+                for i in range(2, len(uvi)):
+                    gr_uv[gr_atc].append([uv[uvi[0]], uv[uvi[i-1]], uv[uvi[i]]])
+
+            vi = [int(k.split("/")[0])-1 for k in fcs]
+            for i in range(2, len(vi)):
+                gr[gr_atc].append([vi[0], vi[i-1], vi[i]])
+
+    return vtx, gr, gr_uv
+
+
 def read_obj_faces_names_x(fname):
-    a,b,c = read_obj_faces_normals(fname)
-    return a,b
+    # Convenience wrapper: returns only vertices and faces (no normals)
+    a, b, c = read_obj_faces_normals(fname)
+    return a, b
 
-        
+
 def read_obj_faces_normals(fname):
-  #f=open("../beetle/VW new Beetle Polizei.obj","r")
-  f=open(fname,"r")
-  vtx=[]
-  nv=[]
-  fc=[]
-  vnn=[]
-  vfn=[]
-  uv=[]
-  fuv=[]
-  obj=[ ] #[ material_name,[(xyz,xyz,xyz)] ]
-  lns= f.readlines()
-  i=0
-  while i < len(lns):
-    #print lns[i]
-    if len(lns[i])>3: 
-      if lns[i][-2]=="\\" :
-         #print lns[i] 
-         lns[i]=lns[i][0:-2]+lns[i+1]
-         del lns[i+1]
-         i-=1
-    i+=1
-         
-  print len(lns)
-  for l in lns:
-    #if l[0]!='v' : continue 
-    ls=l.split()
-    if len(ls)==0: continue
-    if ls[0]=='v':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       vtx.append((x,y,z) )
+    # Parse OBJ file with normals; returns (vertices, faces, face_normals, uv_faces)
+    f = open(fname, "r")
+    lns = _join_continuation_lines(f.readlines())
+    f.close()
+    print len(lns)
 
-       
-    if ls[0]=='vt':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       uv.append((x,y,z) )
+    vtx = []
+    vnn = []   # vertex normals
+    uv  = []
+    fc  = []   # face index list
+    vfn = []   # computed face normals
+    fuv = []   # face UV list
 
-       
-    if ls[0]=='vn':
-       v,x,y,z =l.split()       
-       x,y,z=[float(k) for k in [x,y,z]]
-       normz=  math.sqrt(x*x + y*y +z*z)
-       if normz ==0 :
-            vnn.append((0,0,1) )    
-       vnn.append((x/normz,y/normz,z/normz ) )       
-       #print x,y,z
-  #print "read ",len(vtx), "  vertices"     
-  #return vtx
-##       
-##    if ls[0]=='vn':
-##       v,x,y,z =l.split()
-##       x,y,z=[float(k) for k in [x,y,z]]
-##       nv.append((x,y,z ) )
-    #if ls[0]=='g':
-       #print ls     
-    if ls[0]=='f':
-       fcs=ls[1:]
-       vi=[ int(k.split("/")[0])-1 for k in fcs]
-       
-       ni=[ int(k.split("/")[-1])-1 for k in fcs]
-       
-##       if obj==[]:
-##          obj.append(["DEFAULT",[]])
-##          print obj   
-##       oi=obj[-1][1]
-         
-       vi=[ int(k.split("/")[0])-1 for k in fcs ]
-       if len(uv)>0:
-         uvi=[ int(k.split("/")[1])-1 for k in fcs ]       
-         for i in range(2,len(uvi)):
-             fuv.append( [ uv[0],uv[i-1],uv[i] ] )         
+    for l in lns:
+        ls = l.split()
+        if len(ls) == 0:
+            continue
 
-       
-       for i in range(2,len(vi)):
-           fc.append( [ vi[0],vi[i-1],vi[i] ] )         
-           vfn.append( znormal(vtx[ vi[0]],  vtx[ vi[i-1]], vtx[i] )  )
-                      
-  return vtx,fc,vfn,fuv
+        if ls[0] == 'v':
+            # Vertex position
+            v, x, y, z = l.split()
+            vtx.append((float(x), float(y), float(z)))
 
-##       try:
-##          ni=[ int(k.split("/")[-1])-1 for k in fcs]
-##       except:
-##          print l
-##          return None
-##       #print vi, ni
-##       for i in range(2,len(vi)):
-##           p1,p2,p3= vtx[vi[0]],vtx[vi[i-1]],vtx[vi[i] ]
-##           n1,n2,n3= nv[ni[0]] ,nv[ni[i-1]],nv[ni[i] ]
-##           #print n1,n2,n3
-##           #verifica a normal
-##           ns=znormal(p1,p2,p3)
-##           if n1[0]*ns[0] +n2[1]*ns[1]+n3[2]*ns[2]>0:
-##                 oi.append( (p1,p2,p3 ) )
-##           else:
-##                 oi.append( (p3,p2,p1 ) )  
-##       
-##    if ls[0]=='usemtl':
-##       #novo objeto
-##       obj.append([ls[1],[] ])
-##       print obj[-1]
- 
-#vo_replace_get("../dumb.obj","../tmp2.obj",[]   )              
-##gv,gf=read_obj_faces("../teto.obj" )    
-##print len(gv)
-##print gv[3],gf[3]
+        elif ls[0] == 'vt':
+            # Texture coordinate
+            v, x, y, z = l.split()
+            uv.append((float(x), float(y), float(z)))
+
+        elif ls[0] == 'vn':
+            # Vertex normal (normalized)
+            v, x, y, z = l.split()
+            x, y, z = float(x), float(y), float(z)
+            normz = math.sqrt(x*x + y*y + z*z)
+            if normz == 0:
+                vnn.append((0, 0, 1))
+            vnn.append((x/normz, y/normz, z/normz))
+
+        elif ls[0] == 'f':
+            # Face: triangulate polygons
+            fcs = ls[1:]
+            vi  = [int(k.split("/")[0])-1 for k in fcs]
+
+            if len(uv) > 0:
+                uvi = [int(k.split("/")[1])-1 for k in fcs]
+                for i in range(2, len(uvi)):
+                    fuv.append([uv[0], uv[i-1], uv[i]])
+
+            for i in range(2, len(vi)):
+                fc.append([vi[0], vi[i-1], vi[i]])
+                vfn.append(znormal(vtx[vi[0]], vtx[vi[i-1]], vtx[vi[i]]))
+
+    return vtx, fc, vfn, fuv
